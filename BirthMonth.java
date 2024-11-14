9cfd78672eec3215b491b90cee9b58af50dd18f4
Matthew <cheeseballbusinessemail@gmail.com>
2024-11-14 22:37:58 +0800

    Fixed the entire codebase

--- BirthMonth.java
+++ BirthMonth.java
@@ -8,43 +8,91 @@ public class BirthMonth implements Runnable {
     private Set<String> group1;
     private Set<String> group2;
     private Set<String> self;
+    private Set<String> union;
+    private Set<String> inter;
+    private Set<String> diff; 
 
     public BirthMonth() {
+        clearScreen();
         group1 = new HashSet<String>();
         group2 = new HashSet<String>();
         self = new HashSet<String>();
     }
 
     void promptUser() {
+        Scanner userInput = new Scanner(System.in);
         for (int i = 1; i <= 3; i++) {
             System.out.print("Enter birth month " + i + ": ");
-            Collections.addAll(group1, getBirthMonth());
+            String month = userInput.nextLine().toUpperCase();
+            if (!isValidBirthMonth(month)) {
+                i--;
+                printErrorMessage("Invalid month! Please try again");
+                continue;
+            }
+            Collections.addAll(group1, month);
         }
-        for (int l = 1; l <= 3; l++) {
-            System.out.print("Enter birth month " + l + ": ");
-            Collections.addAll(group2, getBirthMonth());
+        for (int i = 1; i <= 3; i++) {
+            System.out.print("Enter birth month " + i + ": ");
+            String month = userInput.nextLine().toUpperCase();
+            if (!isValidBirthMonth(month)) {
+                i--;
+                printErrorMessage("Invalid month! Please try again");
+                continue;
+            }
+            Collections.addAll(group2, month);
         }
-        System.out.println(group1);
-        System.out.println(group2);
-        System.out.println(group1.addAll(group2));
+        printSets();
+        System.out.print("Enter your birth month: ");
+        Collections.addAll(self, getBirthMonth());
+        if (union.containsAll(self)) {
+            System.out.println("You have the same birth month with one of your classmates.");
+        }
+        userInput.close();
     }
 
-    String getBirthMonth() {
+
+    private void printSets() {
+        System.out.println("Group 1: " + group1);
+        System.out.println("Group 2: "+ group2);
+        union = new HashSet<String>(group1);
+        union.addAll(group2);
+        inter = new HashSet<String>(group1);
+        inter.retainAll(group2);
+        diff = new HashSet<String>(group1);
+        diff.removeAll(group2);
+        System.out.println("Union: "+ union);
+        System.out.println("Intersection: " + inter);
+        System.out.println("Difference: " + diff);
+    }
+
+
+    private String getBirthMonth() {
         Scanner userInput = new Scanner(System.in);
-        String BirthMonth = userInput.nextLine().toUpperCase();
-        for (ListOfBirthMonths month : ListOfBirthMonths.values()) {
-            if (BirthMonth == month.toString()) {
-                return BirthMonth;
+        while (true) {
+            String userMonth = userInput.nextLine().toUpperCase();
+            if (!isValidBirthMonth(userMonth)) {
+                userInput.nextLine();
+                printErrorMessage("Invalid month! Please try again");
+                continue;
+            }
+            userInput.close();
+            return userMonth;
+        }
+    }
+
+    private boolean isValidBirthMonth(String userMonth) {
+        for (ListOfBirthMonths month: ListOfBirthMonths.values()) {
+            if (month.name().equals(userMonth)) {
+                return true;
             }
         }
-        return "Invalid month! Please try again";
+        return false;
     }
 
     private void printErrorMessage(String errorMessage) {
         clearScreen();
         System.out.print(errorMessage);
         char[] characters = {'.', ' ', '.', ' ', '.', ' ', '.'};
-
         for(int i = 0; i < characters.length; i++) {
             System.out.print(characters[i]);
             run();
